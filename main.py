813e9d68fb92df71631efa3a47343c570ed2b38f
David Tejuosho <tejuoshodavid@gmail.com>
2023-02-15 15:34:14 -0600

    Refactor

--- main.py
+++ main.py
@@ -5,58 +5,19 @@ from datetime import datetime
 import pandas as pd
 
 
-def main(sections):
-    sections_tables = [run_for_section(section) for section in sections]
-    for table in sections_tables:
-        print(table)
-        # with open(f"{table.name}.csv", "w+") as csv:
-        #     csv.write(table.to_csv())
-
-
-full_data_frame = pd.read_csv("Graduating Student data/survey_data.csv")
-
-# Store MetaData
-full_question, meta_data = full_data_frame.loc[0:1].values.tolist()  # Consider converting to df.pop
-
-# drop meta_data from table
-full_data_frame = full_data_frame.drop([0, 1])
-
-# transform string dates to datetime dates
-full_data_frame["RecordedDate"] = pd.to_datetime(full_data_frame["RecordedDate"], format="%Y-%m-%d %H:%M:%S")
-
-with open("section_config.json") as file:
-    sections_config = json.load(file)
-
-
 def in_year(dataframe, year):
     return (pd.to_datetime(f"{year}-08-01") <= dataframe["RecordedDate"]) & (
             dataframe["RecordedDate"] <= pd.to_datetime(f"{year + 1}-06-01"))
 
 
-current_year = datetime.now().year
-years = list(range(2019, current_year))  # TODO Remember to add for date past August if program can be run past August
-by_year: dict[int, pd.DataFrame] = {year: full_data_frame[in_year(full_data_frame, year)] for year in years}
-with open("satisfied_map.json", "r") as json_file:
-    satisfied_map: dict[str, int] = json.load(json_file)
-
-
-# Q10_5_Quality : University Ministry
-
-
-# Actual records start at label [2]
-# Please rate the quality and level of availability of the following
-# co/extra-curricular programs a... - Did you participate in the following types of programs? - Student clubs and
-# organizations
-
-
 def mean_frame_for_sub_question(section) -> pd.DataFrame:
-    sub_questions = section["sub_questions"]
+    question_export_tags = section["sub_questions"]
 
-    def get_mean_values_for_subquestion(year_values, sub_question):
-        return year_values[sub_question].dropna().map(satisfied_map.get).mean()
+    def get_mean_values_for_sub_question(year_values, question_export_tag):
+        return year_values[question_export_tag].map(score_map.get).mean()
 
     def get_mean_values_for_year(year):
-        return [get_mean_values_for_subquestion(by_year[year], sub_question) for sub_question in sub_questions]
+        return [get_mean_values_for_sub_question(by_year[year], sub_question) for sub_question in question_export_tags]
 
     all_vals = {year: get_mean_values_for_year(year) for year in years}
     to_return = pd.DataFrame(all_vals, index=sub_questions).round(2)
@@ -68,14 +29,14 @@ def mean_frame_for_sub_question(section) -> pd.DataFrame:
 def run_for_section(section):
     function_dict = {"freq": freq_frame_for_subquestion, "mean": mean_frame_for_sub_question}
 
-    section_frame = function_dict.get(section["type"], sys.exit)(section)
 
-    section_frame.name = section["question"]
-    return section_frame
+def section_type_error(section):
+    return pd.DataFrame({"": [f"Incorrect section type: '{section['type']}' for question: '{section['question']}'"]},
+                        index=["Error"])
 
 
-def freq_frame_for_subquestion(section):
-    section_data = [freq_for_sub_question(sub_question, section["freq_keys"]) for sub_question in
+def freq_frame_for_sub_question(section):
+    section_data = [freq_for_sub_question(question_export_tag, section["freq_keys"]) for question_export_tag in
                     section["sub_questions"]]
     section_frame: pd.DataFrame = pd.concat(section_data)
     section_frame.columns.names = [None, "Question"]
@@ -90,12 +51,12 @@ def value_frequency_from_name_by_year(year, name):
     return frame_of_frequencies.join(frame_of_frequency_rates, rsuffix="%").applymap(int)
 
 
-def freq_for_sub_question(sub_question: str, freq_keys: list[str]):
+def freq_for_sub_question(question_export_tag: str, freq_keys: list[str]):
     def key_frequency_values_in_year(year):
-        frequency_values = value_frequency_from_name_by_year(year, sub_question)
+        frequency_values = value_frequency_from_name_by_year(year, question_export_tag)
         frequency_values.columns = [f"#'{freq_keys_to_string(freq_keys)}'", f"%'{freq_keys_to_string(freq_keys)}'"]
         yes_frequency_values: pd.Series = frequency_values.loc[freq_keys].sum()
-        yes_frequency_values.name = sub_question
+        yes_frequency_values.name = get_question(question_export_tag)
         transposed_frame = yes_frequency_values.to_frame().transpose()
         transposed_frame.columns = pd.MultiIndex.from_product([[year], transposed_frame.columns])
         return transposed_frame
@@ -110,6 +71,47 @@ def freq_keys_to_string(freq_keys: list):
         return freq_keys[0]
     else:
         return " or ".join(freq_keys)
+
+
+full_data_frame = pd.read_csv("Graduating Student data/survey_data.csv")
+
+# Store MetaData
+full_questions: list[str]
+meta_data: list[str]
+full_questions, meta_data = full_data_frame.loc[0:1].values.tolist()  # Consider converting to df.pop
+
+# drop meta_data from table
+full_data_frame = full_data_frame.drop([0, 1])
+
+# transform string dates to datetime dates
+full_data_frame["RecordedDate"] = pd.to_datetime(full_data_frame["RecordedDate"], format="%Y-%m-%d %H:%M:%S")
+
+current_year = datetime.now().year
+years = list(range(2019, current_year))  # TODO Remember to add for date past August if program can be run past August
+by_year: dict[int, pd.DataFrame] = {year: full_data_frame[in_year(full_data_frame, year)] for year in years}
+
+with open("config.json") as file:
+    config = json.load(file)
+    sections_config = config["section_config"]
+    score_map: dict[str, int] = config["score_map"]
+
+
+def run_for_section(section):
+    function_dict = {"freq": freq_frame_for_sub_question, "mean": mean_frame_for_sub_question}
+
+    section_frame = function_dict.get(section["type"], section_type_error)(section)
+
+    section_frame.name = section["question"]
+    return section_frame
+
+
+def main(sections):
+    sections_tables = [run_for_section(section) for section in sections]
+    with open("output.html", "w") as html:
+        for table in sections_tables:
+            print(table)
+            html.write(f"<h2>{table.name}</h2>")
+            html.write(table.to_html(bold_rows=False))
 
 
 main(sections_config)
