28526ddf735cea51d807272da3b3db1cfedb2d82
David Tejuosho <tejuoshodavid@gmail.com>
2023-03-28 18:59:45 -0500

    Completed edit/create form

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ def create_config():
         with open(f"configs/{config_name}.json") as json_file:
             config = json.load(json_file)
             tables = config["section_config"]
-            print(tables)
     return render_template("enter-config.jinja", tables=tables or [], title=config_name or "New Config/Report")
 
 
@@ -49,12 +48,9 @@ def generate_tables():
 
     TableGenerator.configure_generator(config)
     tables = TableGenerator.generate_tables()
-    string = ""
-    table_dict = {table.name: table.to_html(bold_rows=False) for table in tables}
-    for table in tables:
-        string += f"<h2>{table.name}</h2>\n{table.to_html(bold_rows=False)}\n"
+    rendered_tables = {table.name: table.to_html(bold_rows=False) for table in tables}
 
-    return render_template("any.html", content=string)
+    return render_template("generated.jinja", tables=rendered_tables)
 
 
 if __name__ == "__main__":
