60ebceba77f32cb9f8cef5afe0fa175ea556a0bd
David Tejuosho <tejuoshodavid@gmail.com>
2023-04-13 15:14:50 -0500

    Completed major site functionality -m Completed config update and save logic

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import json
 import os
+from collections import defaultdict
 
 from flask import Flask, render_template, request
 
 from TableGenerator import TableGenerator
 
 app = Flask(__name__, template_folder="templates")
+
+
+# TODO: Replace query parameters with POST requests or path parameters
 
 
 @app.route("/")
@@ -16,29 +20,42 @@ def start_page():
 @app.route("/load-config")
 def load_config():
     listdir = os.listdir("configs")
-    listdir = [file[0:-5] for file in listdir if file.endswith(".json")]
-    print(listdir)
+    listdir = [file.replace(".json", "") for file in listdir if file.endswith(".json")]
     return render_template("load-config.jinja", configs=listdir)
+
+
+@app.route("/save-config", methods=["POST"])
+def save_config():
+    data = request.get_json()
+    config = data["config"]
+    config_name = data["config_name"]
+    with open(f"configs/{config_name}.json", "w+") as json_file:
+        json.dump(config, json_file, indent=4)
+    return "Success"
 
 
 @app.route("/create-config")
 @app.route("/edit-config")
 def create_config():
-    tables = 0
+    config = 0
     config_name = request.args.get("config") if "config" in request.args else None
     if config_name is not None:
         with open(f"configs/{config_name}.json") as json_file:
             config = json.load(json_file)
-            tables = config["section_config"]
-    return render_template("enter-config.jinja", tables=tables or [], title=config_name or "New Config/Report")
+            print(config)
+            print(config["score_map"])
+    return render_template("enter-config.jinja", config=config or {}, title=config_name or "New Config/Report")
 
 
 @app.route("/confirm-config")
-def confirm_config():
-    config_name = request.args.get("config")
-    with open(f"configs/{config_name}.json") as json_file:
-        config = json.load(json_file)
-    return render_template("confirm.jinja", config_name=config_name, config=config)
+def confirm_config(config: dict[str, list | int] = None):
+    if config is None:
+        config_name = request.args.get("config")
+        with open(f"configs/{config_name}.json") as json_file:
+            config = json.load(json_file)
+            return render_template("confirm.jinja", config_name=config_name, config=config)
+    else:
+        return render_template("confirm.jinja", config_name="Unsaved Config", config=config, is_new=True)
 
 
 @app.route("/generate-report")
@@ -51,6 +68,39 @@ def generate_tables():
     rendered_tables = {table.name: table.to_html(bold_rows=False) for table in tables}
 
     return render_template("generated.jinja", tables=rendered_tables)
+
+
+def config_form_to_json(form_data: dict[str, str]):
+    tables: defaultdict[str, int | dict[str, str | list]] = defaultdict(lambda: defaultdict(list))
+    score_map = {}
+    new_config = {"section_config": [], "score_map": score_map}
+
+    for key, value in form_data.items():
+        key = key.replace("filter-type", "filterType")
+        split_key = key.split("-")
+        if len(split_key) == 1:
+            score_map[key] = int(value)
+        elif len(split_key) == 2:
+            i, key = split_key
+            if "filter" in key:  # If this value is a filter, we need to add the filter type to the value
+                if key == "filter":  # Ignore the filter type during iteration because it's already added to filter
+                    if value := value.strip():
+                        tables[i][key] = form_data[f"{i}-filter-type"] + value
+                continue
+            tables[i][key] = value
+        elif len(split_key) == 3:
+            i, j, key = split_key
+            tables[i][f"{key}s"].append(value)
+
+    for table in tables.values():
+        new_config["section_config"].append(table)
+    print(json.dumps(new_config, indent=4))
+    return new_config
+
+
+@app.route("/process-entry", methods=["POST"])
+def process_form():
+    return confirm_config(config_form_to_json(request.form))
 
 
 if __name__ == "__main__":
